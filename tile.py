9d161e9eff826c47d4da8fb0d51987742e732a71
Kanishk Rana <32814336+Guardian99@users.noreply.github.com>
2018-10-08 14:52:51 +0530

    Comments for readability

--- tile.py
+++ tile.py
@@ -3,6 +3,7 @@ d=0
 move1=0
 move2=0
 move=0
+#actual tile values of tic tac toe which change state to '1' when occupied
 tile1=0
 tile2=0
 tile3=0
@@ -12,7 +13,8 @@ tile6=0
 tile7=0
 tile8=0
 tile9=0
-
+#player id 
 player1=1
 player2=2
+#the first turn goes everytime to player 1
 turn=player1
